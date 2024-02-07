0589619baf55ce4e0eed978762f8300beb3af35f
Harsh Gandhi <harsh.gandhi@seaflux.tech>
2024-02-07 12:52:47 +0530

    random number generated

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import GenerateNumber from './GenerateNumber'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <GenerateNumber title="Random Number Generator" button="Generate Random Number" />
+      </>
+  )
+}
+
+export default App
---
+++ src/GenerateNumber.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+
+export default function GenerateNumber({title, button}) {
+
+    const [randomNumber, setRandomNUmber] = useState(null)
+
+    const generateRandomNumber = ()=>{
+        const random = Math.floor(Math.random()*100) + 1;
+        setRandomNUmber(random)
+    }
+
+    let generatedNumber = randomNumber % 2 === 0;
+    console.log(generatedNumber);
+
+
+  return (
+    <>
+      <div>
+        <h1>{title}</h1>
+        <p>
+          <button onClick={generateRandomNumber}>{button}</button>
+        </p>
+        <p>Generated Number: {randomNumber}</p>
+        {generatedNumber ? <p>{randomNumber} is even.</p> : <p>{randomNumber} is odd.</p>}
+      </div>
+    </>
+  );
+}
